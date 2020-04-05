6b738e97173def06f54fed8edb28d90cf8032f37
Alejandro2134 <zapatamolinaalejandro@gmail.com>
2020-04-05 16:48:40 -0500

    App ready

--- pokedex/src/components/App.js
+++ pokedex/src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Home from '../pages/Home';
+import PokemonInfo from '../pages/PokemonInfo';
+
 function App () {
     return (
         <BrowserRouter>
             <Switch>
-                
+                <Route path="/" component={Home}/>
+                <Route path="/:pokemonId/info" component={PokemonInfo}/>
             </Switch>
         </BrowserRouter>
     );
